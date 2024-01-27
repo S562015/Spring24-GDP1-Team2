e335f30c79b2a60cceab2ae252f64b8efc789ea5
shivakumarreddy <b.skreddy1997@gmail.com>
2024-01-26 20:47:31 -0600

    Shiva Kumar Reddy Bakkannagari- Team02-01/26/2024 -added routing config

--- talenthunt/src/App.js
+++ talenthunt/src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
-
+import { Link, Route } from 'react-router-dom';
+import Home from "./components/home/home";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p className="App-link">TalentHunt</p>
-      </header>
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+        <Route exact path="/" component={Home} />
+        {/*<Route path="/about" component={About} />*/}
+        {/*<Route path="/contact" component={Contact} />*/}
     </div>
   );
 }
+
+
 
 export default App;
