1b730b658046fe67563933f8f671c98a37d7ea3a
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2024-01-29 10:00:36 -0600

    Anusha Gurijala - Team-02 - 1/29/2024 - added custom components

---
+++ talenthunt/src/components/textField/index.js
@@ -0,0 +1,21 @@
+import { TextField } from "@mui/material";
+ 
+export const InputField = ({
+  id = "outlined-basic",
+  type = "text",
+  label = "Enter value",
+  variant,
+  ...rest
+}) => {
+  return (
+<>
+<TextField
+        id={id}
+        label={label}
+        variant={variant}
+        type={type}
+        {...rest}
+      />
+</>
+  );
+};
