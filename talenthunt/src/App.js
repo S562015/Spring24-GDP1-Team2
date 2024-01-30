6c77c5c928c0bdbf1495d1eb7fca5972400d2e40
bonakash <bonthuakash1@gmail.com>
2024-01-29 22:19:17 -0600

    Akash Bonthu -Team02 -01/29/2024 -added routes and themes

--- talenthunt/src/App.js
+++ talenthunt/src/App.js
@@ -2,12 +2,17 @@ import "./App.css";
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
 import Navbar from "./components/navbar";
+import Login from "./pages/login";
+import SignUp from "./pages/signup";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
       {/*<Home />*/}
     </div>
