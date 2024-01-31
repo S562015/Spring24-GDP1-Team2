800ddd54464a4c3372bd7734c52e8ca5d9fe4301
shivakumarreddy <b.skreddy1997@gmail.com>
2024-01-30 18:20:56 -0600

    Shiva Kumar Reddy Bakkannagari - Team02 - 01/30/2024 "adding mongoose and creating organizationModel.js"

--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
-//import mongoose from "mongoose";
-import cors from 'cors';
+import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
 
-// TODO:check what is config means
-app.use(bodyParser.json({limit:"30mb", extended:true}));
-app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 //middleware
 const PORT = process.env.PORT || 5000;
 // ADD PORT IS LOCAL
-app.listen(PORT,()=>console.log(`Server running on port :${PORT}`))
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port :${PORT}`)),
+  )
+  .catch((error) => console.log(error));
