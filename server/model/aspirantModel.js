1d5ef9389d94e151f2959f80c61214426e49f0c2
S562015 <s562015@nwmissouri.edu>
2024-02-02 22:58:44 -0600

    Nagarjuna Reddy Kunkala - Team 02 - 02/02/2024 - Added personal information to asperant schema

--- server/model/aspirantModel.js
+++ server/model/aspirantModel.js
@@ -37,6 +37,58 @@ const aspirantSchema = new Schema({
     zip:{
       type: String,
     }
+  },
+  education:[
+    {
+      schoolName:{ 
+        type: String 
+      },
+      degree:{ 
+        type: String 
+      },
+      major:{ 
+        type: String 
+      },
+      grade:{ 
+        type: Number 
+      },
+      from:{ 
+        type: Date, 
+        format: 'MMM-yyyy' 
+      },
+      to:{ 
+        type: Date, 
+        format: 'MMM-yyyy' 
+      }
+    }
+  ],
+  skills:[
+    {
+      skill:{
+        type: String
+      },
+      level:{ 
+        type: String,
+        enum: ['Expert','Moderate','Basic']
+      }
+    }
+  ],
+  email:{
+    type: String
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  },
+  applicationDate:{
+    type: Date,
+    default: Date.now,
   }
 });
 
