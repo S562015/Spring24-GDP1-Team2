3c1075917b08891f2bfbe44c35a8e7c9442a2e53
saisumanthkorada <137339004+saisumanthkorada@users.noreply.github.com>
2024-02-05 21:47:54 -0600

    SaiSumanthKorada -Team02 -01/05/2024 -modified application controller to support postman

--- server/controller/applicationController.js
+++ server/controller/applicationController.js
@@ -1,4 +1,4 @@
-import ApplicationModel from "../model/aspirantModel.js";
+import ApplicationModel from "../model/applicationModel.js";
 
 export const getApplication = async (req, res) => {
   try {
@@ -6,5 +6,19 @@ export const getApplication = async (req, res) => {
     res.status(200).json(organizations);
   } catch (error) {
     res.status(404).json({ message: error.message });
+  }
+};
+export const createApplication = async (req, res) => {
+  const log = new ApplicationModel({
+    aspirantId: req.body.aspirantId,
+    jobId: req.body.jobId,
+    applicationDate: req.body.applicationDate,
+    status: req.body.status,
+  });
+  try {
+    const savedLog = await log.save();
+    res.json(savedLog);
+  } catch (err) {
+    res.json({ message: err });
   }
 };
