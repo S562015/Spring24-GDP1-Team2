3f4e438951f17fd23d761c7b5657f4f5c8190274
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2024-02-05 22:55:17 -0600

    Anusha Gurijala- Team-02 - 2/05/2024 - created post method for job in jobController

--- server/controller/jobController.js
+++ server/controller/jobController.js
@@ -8,3 +8,18 @@ export const getJobDetails = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const createJobPost = async (req, res) => {
+  const log = new JobModel({
+    employerId: req.body.employerId,
+    title: req.body.title,
+    qualificationRequired: req.body.qualificationRequired,
+    jobDescription: req.body.jobDescription,
+  });
+  try {
+    const savedLog = await log.save();
+    res.json(savedLog);
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
