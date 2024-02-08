c66162107f242c747eb0056fa3a28d29c2117d06
saisumanthkorada <137339004+saisumanthkorada@users.noreply.github.com>
2024-02-07 22:09:05 -0600

    SaiSumanthKorada -Team02 -02/07/2024 -Modified firebase file

--- talenthunt/src/firebase.js
+++ talenthunt/src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDiUlY68W9Li_0EIkmdGdzD7nvqCT9kHnY",
-  authDomain: "my-auth-test-fbd48.firebaseapp.com",
-  projectId: "my-auth-test-fbd48",
-  storageBucket: "my-auth-test-fbd48.appspot.com",
-  messagingSenderId: "1078604952662",
-  appId: "1:1078604952662:web:5d0b908439cfb5684ab7f7",
+  apiKey: "AIzaSyCPW7T4bl3SmFfsm5hia3Klp-1L0KCUEuA",
+  authDomain: "talenthunt-e4d23.firebaseapp.com",
+  projectId: "talenthunt-e4d23",
+  storageBucket: "talenthunt-e4d23.appspot.com",
+  messagingSenderId: "978558218454",
+  appId: "1:978558218454:web:b6517622538e092e41d977",
+  measurementId: "G-D1D9F8QY32",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
