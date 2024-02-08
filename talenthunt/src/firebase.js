2bd7df4a6724a5187dddcbd081c9c47cf888ad4f
Dinesh Racharla <96189870+DineshRacharla468@users.noreply.github.com>
2024-02-07 22:01:07 -0600

    Dinesh Racharla- Team 02  - 07/2/2024h - adding applicationRoutes

---
+++ talenthunt/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiUlY68W9Li_0EIkmdGdzD7nvqCT9kHnY",
+  authDomain: "my-auth-test-fbd48.firebaseapp.com",
+  projectId: "my-auth-test-fbd48",
+  storageBucket: "my-auth-test-fbd48.appspot.com",
+  messagingSenderId: "1078604952662",
+  appId: "1:1078604952662:web:5d0b908439cfb5684ab7f7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
