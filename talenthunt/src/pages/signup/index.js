3ed643249a0f9c5c6aa0b8c8c901786e2290dd93
S562015 <s562015@nwmissouri.edu>
2024-02-09 22:23:39 -0600

    Nagarjuna Reddy Kunkala - Team 02 - 02/09/2024 - Updated Employer form in sineup page

--- talenthunt/src/pages/signup/index.js
+++ talenthunt/src/pages/signup/index.js
@@ -119,6 +119,87 @@ const SignUp = () => {
       </Grid>
     </Box>
   );
+
+  const renderEmployerForm = () => (
+    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <InputField
+            margin="normal"
+            required
+            fullWidth
+            id="first"
+            label="First Name"
+            name="First Name"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <InputField
+            margin="normal"
+            required
+            fullWidth
+            id="last-name"
+            label="Last Name"
+            name="Last Name"
+          />
+        </Grid>
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="orgisation-name"
+          label="Orgisation Name"
+          name="Orgisation Name"
+        />
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="userName"
+          label="User Name"
+          name="userName"
+        />
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email"
+          name="email"
+          type={"email"}
+        />
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="password"
+          label="Password"
+          name="email"
+          type={"password"}
+        />
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="role"
+          label="Role"
+          name="role"
+        />
+      </Grid>
+      <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+        Register
+      </Button>
+      <Grid container>
+        <Grid item xs>
+          <Link href="#" variant="body2">
+            Already have an account? SignIn
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+
   return (
     <Container component={"main"} maxWidth={"md"}>
       <CssBaseline />
@@ -158,7 +239,7 @@ const SignUp = () => {
           {renderAspirantForm()}
         </TabPanel>
         <TabPanel value={tabIndex} index={1}>
-          {/* {renderEmployerForm()} */}
+          {renderEmployerForm()}
         </TabPanel>
       </Box>
     </Container>
