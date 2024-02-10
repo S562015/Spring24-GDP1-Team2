2325adc3c1551fa0e3c6291d0736dbe1e6031fbc
saisumanthkorada <137339004+saisumanthkorada@users.noreply.github.com>
2024-02-09 22:08:54 -0600

    SaiSumanthKorada -Team02 - 02/09/2024 -Updated signup page

--- talenthunt/src/pages/signup/index.js
+++ talenthunt/src/pages/signup/index.js
@@ -8,8 +8,21 @@ import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import * as React from "react";
 import background from "../../assets/login.png";
+import { Tab, Tabs } from "@mui/material";
+import AssignmentIndOutlinedIcon from "@mui/icons-material/AssignmentIndOutlined";
+import { a11yProps } from "../../utils";
+import BusinessOutlinedIcon from "@mui/icons-material/BusinessOutlined";
+import { TabPanel } from "../../components/tabPanel/TabPanel";
+import { useState } from "react";
+import { Container } from "@mui/system";
+import BasicDatePicker from "../../components/DatePicker/DatePicker";
 
 const SignUp = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabChange = (event, newValue) => {
+    setTabIndex(newValue);
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -20,101 +33,48 @@ const SignUp = () => {
   };
 
   return (
-    <Grid container component="main" sx={{ height: "100vh" }}>
+    <Container component={"main"} maxWidth={"md"}>
       <CssBaseline />
-      <Grid
-        item
-        xs={false}
-        sm={4}
-        md={7}
+      <Box
         sx={{
-          backgroundImage: `url(${background})`,
-          backgroundRepeat: "no-repeat",
-          backgroundColor: "5319AC",
-          backgroundSize: "50%",
-          backgroundPosition: "center",
+          my: 8,
+          mx: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
-      />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <Box
-          sx={{
-            my: 8,
-            mx: 4,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <Typography>Start & track your Scheduling Process</Typography>
+        <Tabs
+          value={tabIndex}
+          onChange={handleTabChange}
+          textColor="primary"
+          indicatorColor="secondary"
         >
-          <Typography component="h1" variant="h5">
-            Sign Up
-          </Typography>
-          <Typography>Start & track your Scheduling Process</Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 1 }}
-          >
-            <InputField
-              margin="normal"
-              required
-              fullWidth
-              id="first"
-              label="First Name"
-              name="First Name"
-              autoFocus
-            />
-            <InputField
-              margin="normal"
-              required
-              fullWidth
-              id="last-name"
-              label="Last Name"
-              name="Last Name"
-            />
-            <InputField
-              margin="normal"
-              required
-              fullWidth
-              id="user-name"
-              label="UserName"
-              name="UserName"
-            />
-            <InputField
-              margin="normal"
-              required
-              fullWidth
-              id="orgisation-name"
-              label="Orgisation Name"
-              name="Orgisation Name"
-            />
-            <InputField
-              margin="normal"
-              required
-              fullWidth
-              id="orgisation-name"
-              label="Orgisation Name"
-              name="Orgisation Name"
-            />
-            <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Create an account ? Sign Up
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Grid>
-    </Grid>
+          <Tab
+            icon={<AssignmentIndOutlinedIcon />}
+            iconPosition="start"
+            label="Aspriant"
+            {...a11yProps(0)}
+          />
+          <Tab
+            icon={<BusinessOutlinedIcon />}
+            iconPosition="start"
+            label="Employeer"
+            {...a11yProps(2)}
+          />
+        </Tabs>
+        <TabPanel value={tabIndex} index={0}>
+          {/* {renderAspirantForm()} */}
+        </TabPanel>
+        <TabPanel value={tabIndex} index={1}>
+          {/* {renderEmployerForm()} */}
+        </TabPanel>
+      </Box>
+    </Container>
   );
 };
 
