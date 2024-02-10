d577ef0e66030eb0bb9ecdeff773256ef3fefce0
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2024-02-09 22:18:07 -0600

    Anusha Gurijala - Team-02 - 02/09/2024- added aspirants registration form

--- talenthunt/src/pages/signup/index.js
+++ talenthunt/src/pages/signup/index.js
@@ -32,6 +32,93 @@ const SignUp = () => {
     });
   };
 
+  const renderAspirantForm = () => (
+    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <InputField
+            margin="normal"
+            required
+            fullWidth
+            id="first"
+            label="First Name"
+            name="First Name"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <InputField
+            margin="normal"
+            required
+            fullWidth
+            id="last-name"
+            label="Last Name"
+            name="Last Name"
+          />
+        </Grid>
+        <BasicDatePicker label="Date of Birth" />
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="qualification"
+          label="qualification"
+          name="qualification"
+        />
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="orgisation-name"
+          label="Orgisation Name"
+          name="Orgisation Name"
+        />
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="address-line1"
+          label="Address line 1"
+          name="Address line 1"
+        />
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="address-line1"
+          label="Address line 2"
+          name="Address line 2"
+        />
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="city"
+          label="City"
+          name="City"
+        />
+        <InputField
+          margin="normal"
+          required
+          fullWidth
+          id="zipCode"
+          label="zipCode"
+          name="ZipCode"
+          type={"number"}
+        />
+      </Grid>
+      <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+        Register
+      </Button>
+      <Grid container>
+        <Grid item xs>
+          <Link href="#" variant="body2">
+            Already have an account? SignIn
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
+  );
   return (
     <Container component={"main"} maxWidth={"md"}>
       <CssBaseline />
@@ -68,7 +155,7 @@ const SignUp = () => {
           />
         </Tabs>
         <TabPanel value={tabIndex} index={0}>
-          {/* {renderAspirantForm()} */}
+          {renderAspirantForm()}
         </TabPanel>
         <TabPanel value={tabIndex} index={1}>
           {/* {renderEmployerForm()} */}
