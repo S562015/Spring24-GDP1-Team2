dde7e3c8d682d325a028dc62c5fc2dd630b085d8
S562015 <s562015@nwmissouri.edu>
2024-02-14 22:18:53 -0600

    Nagarjuna Reddy Kunkala - Team 02 - 02/14/2024 - Added helper function

--- talenthunt/src/utils.js
+++ talenthunt/src/utils.js
@@ -4,3 +4,12 @@ export const a11yProps = (index) => {
     "aria-controls": `simple-tabpanel-${index}`,
   };
 };
+
+export const createFromObj = (array) => {
+  let obj = {};
+  array.forEach((val) => {
+    console.log(val[1]);
+    obj[val[0]] = val[1];
+  });
+  return obj;
+};
