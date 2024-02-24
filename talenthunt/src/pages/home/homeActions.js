75f678779649d84246590fadbd0b26cd31b6aa55
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2024-02-23 22:52:24 -0600

    Anusha Gurijala - Team-02 - 02/23/2024 - added createEmployer

--- talenthunt/src/pages/home/homeActions.js
+++ talenthunt/src/pages/home/homeActions.js
@@ -1,9 +1,23 @@
 import { handleGet, handlePost } from "../../utils";
+import { handlePost } from "../../utils";
 
 export const getJobs = (employerInfo) => {
   return async (dispatch) => {
     try {
       const res = await handleGet("/jobs");
+      if (res.status === 200) {
+        return res;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
+
+export const createEmployer = (employerInfo) => {
+  return async (dispatch) => {
+    try {
+      const res = await handlePost("employer/create", employerInfo);
       if (res.status === 200) {
         return res;
       }
