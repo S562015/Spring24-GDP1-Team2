1ede7f1aa51bc9e5292e6722f0ea42c5d1995590
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2024-02-28 22:56:01 -0600

    Anusha Gurijala - Team-02 - 02/28/2024 - added getJobs in homeReducer

--- talenthunt/src/pages/home/homeReducer.js
+++ talenthunt/src/pages/home/homeReducer.js
@@ -1,12 +1,19 @@
-const initialState = {
-
-};
+const initialState = {};
 
 const homeReducer = (state = initialState, action) => {
-    switch (action.type) {
-        default:
-            return state;
-    }
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
 
+export const getJobs = () => {
+  let data;
+  try {
+    handleGet("jobs").then((val) => (data = val));
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
 export default homeReducer;
