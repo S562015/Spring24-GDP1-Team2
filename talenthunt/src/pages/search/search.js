af2b25673a065b8784c4f04d76dbfc90ca77d3ab
saisumanthkorada <137339004+saisumanthkorada@users.noreply.github.com>
2024-02-28 21:23:25 -0600

    SaiSumanth Korada -2/28/2024 -Team02 -updated  search  page

--- talenthunt/src/pages/search/search.js
+++ talenthunt/src/pages/search/search.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { view, store } from "@risingstack/react-easy-state";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
 
-const Search = () => {
-  return (
-    <>
-      <h1> Search Page </h1>
-    </>
-  );
+// this is re-rendered whenever the relevant parts of the used data stores change
+const Search = ({
+  name,
+  description,
+  image_url: imageUrl,
+  search_Result: searchResult,
+}) => {
+  const search = store({ details: false });
+  <Card
+    onClick={() => {
+      search.details = !search.details;
+    }}
+    className="search"
+  >
+    {!search.details && <CardMedia image={imageUrl} className="media" />}
+    <CardContent>
+      <h3>{name}</h3>
+      {search.details ? (
+        <p>{description}</p>
+      ) : (
+        <ul>
+          {searchResult.map((job) => (
+            <li key={job}>{job}</li>
+          ))}
+        </ul>
+      )}
+    </CardContent>
+  </Card>;
 };
-export default Search;
+export default view(Search);
