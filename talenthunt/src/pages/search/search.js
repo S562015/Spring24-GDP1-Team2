f4ac09f6d2796471985c5fc62425f44cb2bda82a
shivakumarreddy <b.skreddy1997@gmail.com>
2024-02-28 22:46:45 -0600

    Shiva Kumar Reddy Bakkannagari -Team02 - 02/28/24 - updating searchCard

--- talenthunt/src/pages/search/search.js
+++ talenthunt/src/pages/search/search.js
@@ -1,36 +1,22 @@
 import React from "react";
-import { view, store } from "@risingstack/react-easy-state";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
 
-// this is re-rendered whenever the relevant parts of the used data stores change
 const Search = ({
   name,
   description,
   image_url: imageUrl,
   search_Result: searchResult,
 }) => {
-  const search = store({ details: false });
-  <Card
-    onClick={() => {
-      search.details = !search.details;
-    }}
-    className="search"
-  >
-    {!search.details && <CardMedia image={imageUrl} className="media" />}
-    <CardContent>
-      <h3>{name}</h3>
-      {search.details ? (
+  return (
+    <Card className="search">
+      <CardMedia image={imageUrl} className="media" />
+      <CardContent>
+        <h3>{name}</h3>
         <p>{description}</p>
-      ) : (
-        <ul>
-          {searchResult.map((job) => (
-            <li key={job}>{job}</li>
-          ))}
-        </ul>
-      )}
-    </CardContent>
-  </Card>;
+      </CardContent>
+    </Card>
+  );
 };
-export default view(Search);
+export default Search;
