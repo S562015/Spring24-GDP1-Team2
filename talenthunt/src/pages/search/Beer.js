f7dafd5043002c1c08a5a0e4b92cef2267ba7f42
saisumanthkorada <137339004+saisumanthkorada@users.noreply.github.com>
2024-03-22 21:35:07 -0500

    SaiSumanth Korada -03/22/2024 -Team02 -created prototype

---
+++ talenthunt/src/pages/search/Beer.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { view, store } from "@risingstack/react-easy-state";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+
+// this is re-rendered whenever the relevant parts of the used data stores change
+const Beer = ({
+  name,
+  description,
+  image_url: imageUrl,
+  food_pairing: foodPairing,
+}) => {
+  const beer = store({ details: false });
+
+  return (
+    <Card
+      onClick={() => {
+        beer.details = !beer.details;
+      }}
+      className="beer"
+    >
+      {!beer.details && <CardMedia image={imageUrl} className="media" />}
+      <CardContent>
+        <h3>{name}</h3>
+        {beer.details ? (
+          <p>{description}</p>
+        ) : (
+          <ul>
+            {foodPairing.map((food) => (
+              <li key={food}>{food}</li>
+            ))}
+          </ul>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default view(Beer);
