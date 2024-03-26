cd1da5044294cb52eae6a975d48d151c955a7536
S562015 <s562015@nwmissouri.edu>
2024-03-25 23:21:13 -0500

    Nagarjuna Reddy Kunkala - Team 02 - 03/25/2024 - modified jobpost page

--- talenthunt/src/pages/jobpost/jobpost.js
+++ talenthunt/src/pages/jobpost/jobpost.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, TextField, Typography } from "@material-ui/core";
 
-const JobPost = () => {
+const JobPostingPage = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = () => {
+    // TODO: Post the job to your backend
+  };
+
   return (
-    <>
-      <h1> Job post Page </h1>
-    </>
+    <div>
+      <Typography variant="h4">Post a Job</Typography>
+      <TextField
+        label="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <TextField
+        label="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <Button variant="contained" color="primary" onClick={handleSubmit}>
+        Post Job
+      </Button>
+    </div>
   );
 };
-export default JobPost;
+
+export default JobPostingPage;
