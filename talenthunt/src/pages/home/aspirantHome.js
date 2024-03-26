37da17c8e03c7e9d317271af4dfd18c0e58a7e7a
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2024-03-25 23:03:29 -0500

    Anusha Gurijala - Team-02 - 03/25/2024 - updated  aspirantHome

--- talenthunt/src/pages/home/aspirantHome.js
+++ talenthunt/src/pages/home/aspirantHome.js
@@ -12,42 +12,15 @@ const AspirantHome = () => {
     dispatch(getJobs());
   }, [dispatch]);
 
-  const renderJobList = () => {
-    return jobList?.map((job, index) => <JobCard job={job} key={index} />);
-  };
-
   return (
-    <>
-      <div className="section-1">
-        <Grid container spacing={2}>
-          <Grid item md={6}>
-            <div className="p-70">
-              <h2 className="fs-30  home-line1">
-                There are <span className="color-secondary">1,26,366</span> job{" "}
-                <span className="color-secondary">Opportunities</span> for you.
-              </h2>
-              <p className="text-muted fw-600 ">
-                Find Jobs, Employment, and Career opportunities
-              </p>
-            </div>
-          </Grid>
-          <Grid item md={6}>
-            <div>
-              <img src={homeBanner} alt="" style={{ width: "100%" }} />
-            </div>
-          </Grid>
-        </Grid>
+    <div className="aspirant-home">
+      <h1>Welcome to Your Aspirant Home Page</h1>
+      <div className="job-listings">
+        {jobList.map((job, index) => (
+          <JobCard key={index} job={job} />
+        ))}
       </div>
-      <div className="section-2">
-        <h1>
-          {" "}
-          Popular <span className="color-secondary">Jobs</span>{" "}
-        </h1>
-        <div>
-          <div className="popular-jobs">{renderJobList()}</div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
