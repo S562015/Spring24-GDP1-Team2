e0a4ea0e17b5401b14b026ff39cbfa36a948da63
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2024-03-29 23:09:37 -0500

    Anusha Gurijala - Team-02 - 03/29/2024 - Updated AspirantHome page

--- talenthunt/src/pages/home/aspirantHome.js
+++ talenthunt/src/pages/home/aspirantHome.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
-import Grid from "@mui/material/Grid";
-import { getJobs } from "./homeActions";
 import { useDispatch, useSelector } from "react-redux";
+import { getJobs } from "./homeActions";
 import JobCard from "../../components/Card/JobCard";
+import SearchBar from "../../components/SearchBar";
+import FilterPanel from "../../components/FilterPanel";
 
 const AspirantHome = () => {
   const dispatch = useDispatch();
-  const { jobList } = useSelector((state) => state.homeReducer);
+  const { jobList, loading, error } = useSelector((state) => state.homeReducer);
 
   useEffect(() => {
     dispatch(getJobs());
@@ -14,12 +15,31 @@ const AspirantHome = () => {
 
   return (
     <div className="aspirant-home">
-      <h1>Welcome to Your Aspirant Home Page</h1>
-      <div className="job-listings">
-        {jobList.map((job, index) => (
-          <JobCard key={index} job={job} />
-        ))}
-      </div>
+      <header>
+        <h1>Welcome to Your Aspirant Home Page</h1>
+        <SearchBar />
+        <FilterPanel />
+      </header>
+      <main>
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error: {error.message}</p>
+        ) : (
+          <div className="job-listings">
+            {jobList.length > 0 ? (
+              jobList.map((job) => (
+                <JobCard key={job.id} job={job} />
+              ))
+            ) : (
+              <p>No jobs found</p>
+            )}
+          </div>
+        )}
+      </main>
+      <footer>
+        <p>Footer content goes here</p>
+      </footer>
     </div>
   );
 };
