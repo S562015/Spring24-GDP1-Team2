7a31e800d1ce6ff8adffb20f1a07bf12904125a7
saisumanthkorada <137339004+saisumanthkorada@users.noreply.github.com>
2024-04-08 23:38:43 -0500

    SaiSumanth Korada -04/08/2024 -Team02 -Modified signup actions

--- talenthunt/src/pages/signup/signupActions.js
+++ talenthunt/src/pages/signup/signupActions.js
@@ -1,33 +1,34 @@
-import { handleGet, handlePost } from "../../utils";
-import { GET_ALL_JOBS, CREATE_EMPLOYER } from "../../redux/actionType";
+import { handlePost } from "../../utils";
+import { CREATE_EMPLOYER } from "../../redux/actionType";
 
-export const createEmployer = (employerInfo) => {
-  return async (dispatch) => {
-    return handlePost("employer/create", employerInfo)
-      .then((response) => {
-        console.log(response);
-        if (response.status === 200) {
-          dispatch({
-            type: CREATE_EMPLOYER,
-            data: response.data
-          });
-        }
-      })
-      .catch((error) => {
-        console.error(error);
+export const createEmployer = async (employerInfo, dispatch) => {
+  try {
+    const response = await handlePost("employer/create", employerInfo);
+    if (response.status === 200) {
+      dispatch({
+        type: CREATE_EMPLOYER,
+        data: response.data,
       });
-  };
+      return response.data;
+    } else {
+      throw new Error("Failed to create employer");
+    }
+  } catch (error) {
+    console.error("Error creating employer:", error);
+    throw error; // Re-throw error to propagate it to the caller
+  }
 };
 
-export const createAspirant = (aspirantInfo) => {
-  return async (dispatch) => {
-    try {
-      const res = await handlePost("aspirant/create", aspirantInfo);
-      if (res.status === 200) {
-        return res;
-      }
-    } catch (e) {
-      console.log(e);
+export const createAspirant = async (aspirantInfo) => {
+  try {
+    const response = await handlePost("aspirant/create", aspirantInfo);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error("Failed to create aspirant");
     }
-  };
+  } catch (error) {
+    console.error("Error creating aspirant:", error);
+    throw error; // Re-throw error to propagate it to the caller
+  }
 };
