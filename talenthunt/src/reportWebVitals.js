77d2b2f435f077d26a52f686388aabc1979339c0
bonakash <bonthuakash1@gmail.com>
2024-04-10 23:48:42 -0500

    Akash Bonthu -Team02 -04/10/2024- Updated PrivateRoutesLayout.js

--- talenthunt/src/reportWebVitals.js
+++ talenthunt/src/reportWebVitals.js
@@ -1,12 +1,18 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && typeof onPerfEntry === "function") {
+    import("web-vitals")
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      })
+      .catch((error) => {
+        console.error("Error importing web-vitals:", error);
+      });
+  } else {
+    console.error("Invalid argument for onPerfEntry:", onPerfEntry);
   }
 };
 
